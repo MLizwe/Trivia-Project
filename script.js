90426c9b88a0b7f156c8a0509419131937f45e8b
Lizwe Mangobe <lizwemjr@icloud.com>
2025-11-17 11:29:16 -0330

    Added startGame and showQuestion to render first question

--- script.js
+++ script.js
@@ -19,9 +19,35 @@ let currentIndex = 0;
 let score = 0;
 
 //possible functions ***to be expanded***
-function startGame() {};
+function startGame() {
+    currentIndex = 0;
+    score = 0;
+    showQuestion();
+}
+
+function showQuestion() {
+    const q = questions[currentIndex];
+
+    const container = document.getElementById("game-screen");
+    container.innerHTML = `
+        <h2>${q.text}</h2>
+        <div id="answers"></div>
+    `;
+
+    const answersDiv = document.getElementById("answers");
+
+    q.answers.forEach((ans, i) => {
+        const btn = document.createElement("button");
+        btn.textContent = ans;
+        btn.onclick = () => checkAnswer(i + 1);
+        answersDiv.appendChild(btn);
+    });
+}
+
 function showQuestions() {};
 function checkAnswer() {};
 function endGame () {};
+
+startGame();
 
 console.log("JS loaded. Questions ready:", questions.length);
